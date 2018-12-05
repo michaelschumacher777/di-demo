3fac6ff241eb27011aadf0dbeebd55fc588ef521
Michael Schumacher <michael.j.schumacher777@gmail.com>
2018-12-05 09:34:03 -0600

    Adding in the Qualifier where needed...

--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -1,7 +1,11 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
@@ -10,10 +14,8 @@ public class SetterInjectedController {
         return this.getClass().getSimpleName() + " " + greetingService.sayGreeting();
     }
 
-    public GreetingService getGreetingService() {
-        return greetingService;
-    }
-
+    @Autowired
+    @Qualifier("getterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
