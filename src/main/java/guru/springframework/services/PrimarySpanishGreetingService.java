1187f91e355300c17ab83c948721426828c3332a
Michael Schumacher <michael.j.schumacher777@gmail.com>
2018-12-05 10:32:10 -0600

    Adding in profiles

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService{
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
+
     @Override
     public String sayGreeting() {
-        return "Hello - primary Greeting Service";
+        return "Spanish Greeting";
     }
 }
