115329980bdda333a73778b4272a9a53b836da46
Michael Schumacher <michael.j.schumacher777@gmail.com>
2018-12-05 08:53:39 -0600

    Multiple types of DI with tests

---
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -0,0 +1,20 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+
+    public GreetingService getGreetingService() {
+        return greetingService;
+    }
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+}
---
+++ src/test/java/guru/springframework/Controllers/GetterInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.Controllers;
+
+import guru.springframework.controllers.SetterInjectedController;
+import guru.springframework.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetterInjectedControllerTest {
+
+    private SetterInjectedController setterInjectedController;
+
+    @Before
+    public void setup() throws Exception {
+        this.setterInjectedController = new SetterInjectedController();
+        this.setterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, setterInjectedController.sayHello());
+    }
+}
